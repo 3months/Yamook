5ce0a3e8d53022d4f12f580286a43f3a2915df4f
Josh McArthur <joshua.mcarthur@gmail.com>
2011-09-18 14:38:07 +1200

    Add really basic sinatra app

---
+++ yamook.rb
@@ -0,0 +1,26 @@
+class Yamook < Sinatra::Base
+  set :cache, Dalli::Client.new
+
+  post '/' do
+    @message = message_from_payload(params[:payload])
+    broadcast_on(:yammer, @message)
+  end
+
+  private
+  
+  def message_from_payload(payload = [])
+    @payload = JSON.parse(payload)
+    @template = Liquid::Template.parse(ENV['message_template'])
+    @template.render({
+      :repository => @payload["repository"]["name"],
+      :user => @payload["commits"].last["author"]["name"],
+      :message => @payload["commits"].last["message"],
+      :url => @payload["commits"].last["url"]
+    })
+  end
+
+  def broadcast_on(provider = :yammer, message)
+    #Do the actual broadcasting here, for now, just print the message
+    render message
+  end
+end
